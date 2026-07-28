fdf13a9301c3f8c101fc3c89c6a0076b27b088ae
Joe <jblackman1609@gmail.com>
2026-07-27 20:12:20 -0500

    Added MealPlanner.SharedKernel/Domain classes.

---
+++ MealPlanner.SharedKernel/Domain/AggregateRoot.cs
@@ -0,0 +1,14 @@
+public abstract class AggregateRoot : Entity
+{
+    protected AggregateRoot(Guid id) : base(id)
+    {
+    }
+
+    protected IList<DomainEvent> _domainEvents;
+    public IReadOnlyCollection<DomainEvent> DomainEvents => _domainEvents.ToList();
+    protected void AddDomainEvent(DomainEvent domainEvent)
+    {
+        _domainEvents ??= new List<DomainEvent>();
+        _domainEvents.Add(domainEvent);
+    }
+}
---
+++ MealPlanner.SharedKernel/Domain/Entity.cs
@@ -0,0 +1,9 @@
+public abstract class Entity
+{
+    public Guid Id { get; protected set; }
+
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+}
