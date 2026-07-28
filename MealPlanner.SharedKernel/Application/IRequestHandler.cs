945a526af1489e00e583b719a9e46fd031e540d3
Joe <jblackman1609@gmail.com>
2026-07-27 20:21:12 -0500

    Added MealPlanner.SharedKernel/Application.

---
+++ MealPlanner.SharedKernel/Application/IRequestHandler.cs
@@ -0,0 +1,4 @@
+public interface IRequestHandler<in TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken = default);
+}
